a573ae7e31208f690b6458a50dc19ac3b89f45ea
AlexeyVolkov2023 <alexv0lk0v2024@yandex.ru>
2024-08-10 09:55:24 +0300

    Create class Volunter , SocialNetwork

---
+++ backend/src/HelpAnimal.Domain/Models/SocialNetwork.cs
@@ -0,0 +1,13 @@
+﻿namespace HelpAnimal.Domain.Models;
+
+public class SocialNetwork
+{
+    public string Name { get;private set; } // Название социальной сети
+    public string Link { get;private set; } // Ссылка на профиль
+
+    public SocialNetwork(string name, string link)
+    {
+        Name = name;
+        Link = link;
+    }
+}
